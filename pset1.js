b6b35febbdee74019ebeb9b803ebdd07c429b1d9
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-11-17 23:53:02 -0500

    Create pset1.js

---
+++ pset1.js
@@ -0,0 +1,112 @@
+/*
+    @TODO: READ THIS!
+    
+    
+    Implement the following functions __RECURSIVELY__
+    NOTE: autoplay has been TURNED OFF to protect against
+    accidental infinite loops. Feel free to turn back on but
+    do so AT YOUR OWN RISK.
+*/
+
+/*  1
+    @function factorial
+    @param n {number}
+    @returns {number}
+    @description:
+        - factorial is defined to be n!
+            or, 5! = 5*4*3*2*1
+*/
+
+/*  2
+    @function getInRange
+    @param s {number}
+    @param e {number}
+    @returns {array}
+    @description:
+        - generate numbers in range
+            for example getInRange(2,5) 
+            should give us back [2,3,4,5]
+    
+    HINT:
+        - remember to account for what happens if s > e!
+*/
+
+/*  3
+    @function isEven
+    @param n {number}
+    @returns {boolean}
+    @description:
+        - determine without using % operator or Math.floor(), etc
+            whether or not a number is even
+*/
+
+/*  4
+    @function pow
+    @param b {number}
+    @param n {number}
+    @returns {number}
+    @description:
+        - determine without using anything other than multiplication
+            the value of b^n
+*/
+
+/*  5
+    @function multiply
+    @param a {number}
+    @param b {number}
+    @returns {number}
+    @description:
+        - determine without using multiplication operator the product
+            of a and b
+*/
+
+/*  6
+    @function reverse
+    @param s {string}
+    @returns {string}
+    @description:
+        - recursively reverse a string
+*/
+
+/*  7
+    @function isPalindrome
+    @param s {string}
+    @returns {boolean}
+    @description:
+        - recursively determine if a string is a palindrome
+*/
+
+
+/*  8
+    @function map
+    @param arr {array}
+    @param cb {function}
+    @returns {array}
+    @description:
+        - recursively implement map
+*/
+
+
+/*  9
+    @function filter
+    @param arr {array}
+    @param cb {function}
+    @returns {array}
+    @description:
+        - recursively implement filter
+*/
+
+
+/*  10
+    @function reduce
+    @param arr {array}
+    @param cb {function}
+    @param acc {anything}
+    @returns {anything}
+    @description:
+        - recursively implement reduce
+*/
+
+
+
+
